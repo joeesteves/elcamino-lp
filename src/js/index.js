887fa23fdbe6e2bc84f889d7d9d4e94b16e62e36
ponyesteves <ajesteves@gmail.com>
2017-03-30 15:23:45 -0300

    rc1

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ import {init} from "./init"
 import body from "../views/body.pug"
 require("../styles/_bootstrap.scss")
 require("../styles/stylish-portfolio.css")
+require("../../node_modules/font-awesome/css/font-awesome.css")
 $(function () {
     $("#body").html(body)
     init()
